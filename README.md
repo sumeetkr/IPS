d238e971b4cbb58a967d1f41fbdeeae762cab9ec
Sumeet Kumar <kr.sumeet@gmail.com>
2014-09-10 09:18:57 -0700

    initial commit with basic folder structure

--- README.md
+++
@@ -1,4 +0,0 @@
-IPS
-===
-
-Indoor Positioning System using Infrared beacons and mobile phones with IR sensors.
---
+++ app/src/main/java/sv/cmu/edu/ips/ManchesterDecoder.java
@@ -0,0 +1,7 @@
+package sv.cmu.edu.ips;
+
+/**
+ * Created by sumeet on 9/8/14.
+ */
+public class ManchesterDecoder {
+}
