c0336e243da1eff76bcccf44b992b3ccfd029087
Sumeet Kumar <kr.sumeet@gmail.com>
2014-12-05 09:53:20 -0800

    changed text to button for different types of sensors

--- app/src/main/java/sv/cmu/edu/ips/learners/WekaKNNClassifier.java
+++ app/src/main/java/sv/cmu/edu/ips/learners/WekaKNNClassifier.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.IOException;
 
 import sv.cmu.edu.ips.util.Logger;
+import weka.classifiers.Classifier;
+import weka.classifiers.lazy.IBk;
 import weka.core.Instances;
 
 /**
@@ -22,19 +24,21 @@ public class WekaKNNClassifier {
         try {
             insts = new Instances(new java.io.FileReader(path+"/iris.arff"));
         insts.setClassIndex(insts.numAttributes() - 1);
+        int trainSize = insts.numInstances() * percentSplit / 100;
+        int testSize = insts.numInstances() - trainSize;
+        weka.core.Instances train = new weka.core.Instances(insts, 0, trainSize);
 
-        weka.classifiers.Classifier cl = new weka.classifiers.trees.J48();
+//        weka.classifiers.Classifier cl = new weka.classifiers.trees.J48();
+//        cl.buildClassifier(train);
+
+        Classifier cl = new IBk();
+        cl.buildClassifier(train);
         Logger.log("Performing " + percentSplit + "% split evaluation.");
 
         //randomize the order of the instances in the dataset.
 //        weka.filters.Filter myRandom = new weka.filters.unsupervised.instance.Randomize();
 //        myRandom.setInputFormat(insts);
 //        insts = weka.filters.Filter.useFilter(insts, myRandom);
-
-        int trainSize = insts.numInstances() * percentSplit / 100;
-        int testSize = insts.numInstances() - trainSize;
-        weka.core.Instances train = new weka.core.Instances(insts, 0, trainSize);
-        cl.buildClassifier(train);
 
         int numCorrect = 0;
         for (int i = trainSize; i < insts.numInstances(); i++)
